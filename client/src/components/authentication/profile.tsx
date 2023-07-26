cf92b6c9eee42150c17caef6aff397b90a4a9bd6
Isuru lakshan ketawala <82941889+IsuruLakshan170@users.noreply.github.com>
2023-07-26 23:57:29 +0530

    apply api for login and register in ui section

--- client/src/components/authentication/profile.tsx
+++ client/src/components/authentication/profile.tsx
@@ -1,72 +1,134 @@
+import { useState } from "react";
+import axios from "axios";
+
 function Profile() {
+  const [isChecked, setIsChecked] = useState(false);
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [useremail, setEmail] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordMatch, setPasswordMatch] = useState(true);
+
   const containerRightStyle: React.CSSProperties = {
-    marginTop:'-200px',
+    marginTop: '-200px',
     display: 'flex',
     justifyContent: 'left',
     alignItems: 'center',
     marginLeft: '40px'
   };
+
+  const handleCheckboxChange = (event: any) => {
+    setIsChecked(event.target.checked);
+  };
+
+  const handlePasswordChange = (event: any) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event: any) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleUsernameChange = (event: any) => {
+    setUsername(event.target.value);
+  };
+
+  const handleEmailChange = (event: any) => {
+    setEmail(event.target.value);
+  };
+
+  function handleRegisterClick() {
+    if (password !== confirmPassword) {
+      setPasswordMatch(false);
+    } else {
+      setPasswordMatch(true);
+      const userDetails = {
+        name: username,
+        email: useremail,
+        password: password,
+      };
+      console.log(userDetails);
+      axios
+        .post('YOUR_API_ENDPOINT', userDetails)
+        .then(() => {
+          console.log("User registered successfully!");
+        })
+        .catch((error) => {
+          console.error("Error registering user:", error);
+        });
+    }
+  }
   return (
     <div>
-         <div style={containerRightStyle}>
-        <div className="h-56 grid grid-cols-1 gap-0  mt-0">
-          <div>
-            <h1 style={{ fontSize: '44px', fontWeight: 'bold',textAlign:'left',color:'#001C30' }}>Register</h1>
+      <div style={containerRightStyle}>
+        <div className="h-56 grid grid-cols-1 gap-0 mt-0">
+          <div style={{ textAlign: 'left' }}>
+            <h1 style={{ fontSize: '44px', fontWeight: 'bold', color: '#001C30' }}>Register</h1>
           </div>
-          <div>
-            <h1 style={{textAlign:'left'}}>Please fill in the information below </h1>
+          <div style={{ textAlign: 'left' }}>
+            <h1>Please fill in the information below</h1>
           </div>
-
-          <div className=" mt-2">
-            <input placeholder="Your Name"
+          <div className="mt-2" style={{ textAlign: 'left' }}>
+            <input
+              placeholder="Your Name"
               type="text"
-              style={{ border: '1px solid #7FFFD4	', borderRadius: '5px', height: '50px', width: '300px' }}
+              value={username}
+              onChange={handleUsernameChange} // Update username state on input change
+              style={{ border: '1px solid #7FFFD4', borderRadius: '5px', height: '50px', width: '300px' }}
             />
           </div>
-          <div className=" mt-2">
-            <input placeholder="email"
+          <div className="mt-2" style={{ textAlign: 'left' }}>
+            <input
+              placeholder="Email"
               type="text"
-              style={{ border: '1px solid #7FFFD4	', borderRadius: '5px', height: '50px', width: '300px' }}
+              value={useremail}
+              onChange={handleEmailChange} // Update email state on input change
+              style={{ border: '1px solid #7FFFD4', borderRadius: '5px', height: '50px', width: '300px' }}
             />
           </div>
-          <div className=" mt-2">
-            <input placeholder="Password"
-              type="text"
-              style={{ border: '1px solid #7FFFD4	', borderRadius: '5px', height: '50px', width: '300px' }}
+          <div className="mt-2" style={{ textAlign: 'left' }}>
+            <input
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              style={{ border: '1px solid #7FFFD4', borderRadius: '5px', height: '50px', width: '300px' }}
             />
           </div>
-          <div className=" mt-2">
-            <input placeholder="Confirm Password"
-              type="text"
-              style={{ border: '1px solid #7FFFD4	', borderRadius: '5px', height: '50px', width: '300px' }}
+          <div className="mt-2" style={{ textAlign: 'left' }}>
+            <input
+              placeholder="Confirm Password"
+              type="password"
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+              style={{ border: `1px solid ${passwordMatch ? '#7FFFD4' : 'red'}`, borderRadius: '5px', height: '50px', width: '300px' }}
             />
+            {!passwordMatch && <p style={{ color: 'red', margin: '5px 0', fontSize: '12px' }}>Passwords do not match</p>}
           </div>
-
-
-            <div className="grid grid-cols-2 gap-1 mt-2">
-                <div className="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
-                  <input
-                    className="relative float-left -ml-[1.5rem] mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
-                    type="checkbox"
-                    value=""
-                    id="checkboxDefault" />
-                  <label
-                    className="inline-block pl-[0.15rem] hover:cursor-pointer"
-                    htmlFor="checkboxDefault">
-                    I agree to terms & Policy.
-                  </label>
-                </div>
-              
-            
+          <div className="grid grid-cols-2 gap-1 mt-2">
+            <div className="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]" style={{ textAlign: 'left' }}>
+              <label className="inline-block pl-[0.15rem] hover:cursor-pointer items-center" htmlFor="checkboxDefault">
+                <input
+                  type="checkbox"
+                  id="checkboxDefault"
+                  checked={isChecked}
+                  onChange={handleCheckboxChange}
+                  className="form-checkbox mr-2 h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+                />
+                I agree to terms & Policy.
+              </label>
             </div>
-
-
-            
-            <div style={{ textAlign: 'left' }} className="mt-8">
-           <button style={{padding:'15px',width:'130px'}} className="bg-blue-900 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-        Register
-      </button>
-    </div>
+          </div>
+          <div style={{ textAlign: 'left' }} className="mt-8">
+            <button
+              onClick={handleRegisterClick}
+              style={{ padding: '15px', width: '130px' }}
+              className={`text-white font-bold py-2 px-4 rounded ${isChecked ? 'bg-blue-900 hover:bg-green-700' : 'bg-gray-400'}`}
+              disabled={!isChecked}
+            >
+              Register
+            </button>
+          </div>
         </div>
       </div>
     </div>
