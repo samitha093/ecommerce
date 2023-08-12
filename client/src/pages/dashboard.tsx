2dce1d1d3532f25eb67266b0bd6abb066f764169
Isuru lakshan ketawala <82941889+IsuruLakshan170@users.noreply.github.com>
2023-08-12 09:17:41 +0530

    store api call

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -1,19 +1,23 @@
 import { useState } from "react";
 import AddProduct from "../components/dashboard/addProduct";
 import ProductTable from "../components/dashboard/productTable";
+import axios, { AxiosResponse } from "axios";
+import Toast from "../components/modules/toast";
 
 interface DivStyle {
   backgroundColor: string;
   padding: string;
 }
+
 interface Product {
   id: number;
   name: string;
   description: string;
   categoryId: number;
   price: number;
-  stockQuantity: number;
-  soldQuantity: number;
+  stockQTY: number;
+  soldQTY: number;
+  imageListId:string
 }
 
 function Dashboard() {
@@ -28,7 +32,41 @@ function Dashboard() {
   const [products, setProducts] = useState<Product[]>([]); // Initialize products state as an empty array
   const addProductToList = (product: Product) => {
     setProducts([...products, product]); // Add the new product to the products array
+    console.log(products);
   };
+  function removeProductById(id: number) {
+    const updatedProducts = products.filter(product => product.id !== id);
+    setProducts(updatedProducts); 
+    console.log(products);
+    removeItemFromStore(id);    
+  }
+  function removeItemFromStore(id: number) {
+    const myHost = sessionStorage.getItem('host');
+      axios
+        .delete(`${myHost}/api/v1/products/${id}`)
+        .then((response) => {
+          if (response.status === 200) {
+            Toast.fire({
+              icon: 'success',
+              title: 'Product deleted successfully'
+            });
+            
+            // Perform any necessary updates in your state or UI here after successful deletion
+          } else {
+            Toast.fire({
+              icon: 'error',
+              title: 'Can not delete product'
+            });
+          }
+        })
+        .catch(() => {
+          Toast.fire({
+            icon: 'error',
+            title: 'Can not delete product'
+          });
+        });
+    
+     }
 
   return (
     <div>
@@ -39,7 +77,7 @@ function Dashboard() {
               <AddProduct  onAddProduct={addProductToList}/>
           </div>
           <div className="col-span-6" style={divStyle2}>
-           <ProductTable products={products}/>
+           <ProductTable products={products} removeProductById={removeProductById}/>
           </div>
           </div>
     
