09928f61fb01e8d2f5b69f982c5806db5a9933d4
Isuru lakshan ketawala <82941889+IsuruLakshan170@users.noreply.github.com>
2023-08-15 18:07:45 +0530

    add

--- client/src/components/dashboard/productTable.tsx
+++ client/src/components/dashboard/productTable.tsx
@@ -31,11 +31,10 @@ const ProductTable: React.FC<ProductTableProps> = ({ products,removeProductById,
   return (
     <div className="relative overflow-x-auto">
       <div style={{ textAlign: 'center' }}>
-        <h4 style={{ fontSize: '30px', fontWeight: 'bold', textAlign: 'center', color: '#001C30' }}>All Product</h4>
+        <h4 style={{ fontSize: '30px', fontWeight: 'bold', textAlign: 'center', color: '#001C30' }}>Product Store</h4>
       </div>
-
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <table className="w-full text-sm text-left text-black-900 dark:text-gray-900">
+        <thead className="text-xs text-gray-100 uppercase bg-gray-100 dark:bg-gray-200 dark:text-gray-400">
           <tr>
             <th scope="col" className="px-6 py-3">
               Product name
@@ -68,8 +67,8 @@ const ProductTable: React.FC<ProductTableProps> = ({ products,removeProductById,
         </thead>
         <tbody>
           {products.map((product) => (
-            <tr key={product.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <tr key={product.id} className="bg-white border-b dark:bg-gray-0 dark:border-gray-0">
+              <td className="px-6 py-4">
                 {product.name}
               </td>
               <td className="px-6 py-4">
