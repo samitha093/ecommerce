37ae5bbaa6efee5115c83f3f5b57861896dabcfe
lakshan Pathiraja <lakshanSpathiraja@proton.me>
2023-08-31 23:18:57 +0530

    Update OpenAPIConfig.java

--- products/src/main/java/com/ecommerce/products/config/OpenAPIConfig.java
+++ products/src/main/java/com/ecommerce/products/config/OpenAPIConfig.java
@@ -26,9 +26,9 @@ public class OpenAPIConfig {
     License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
 
     Info info = new Info()
-        .title("Tutorial Management API")
+        .title("Product micro-service")
         .version("1.0")
-        .description("This API exposes endpoints to manage tutorials.").termsOfService("")
+        .description("This API exposes endpoints to Product micro service.").termsOfService("")
         .license(mitLicense);
 
     return new OpenAPI().info(info).servers(List.of(devServer));
